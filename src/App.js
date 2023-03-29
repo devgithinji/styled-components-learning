d132863686e740a24b6e272a9fe02adeae0e3f01
dennis githinji <wakahiad@gmail.com>
2023-03-29 14:39:32 +0300

    inline style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
     return (
         <div>
             <h1>styled components</h1>
+            <h2 style={{color: "red", textTransform: "uppercase"}}>
+                inline styles
+            </h2>
         </div>
     );
 }
