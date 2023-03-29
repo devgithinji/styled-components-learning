638ec024172891609f9b654e24d443c9d5596815
dennis githinji <wakahiad@gmail.com>
2023-03-29 17:53:09 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import './App.css';
-import StyledHeader from "./components/examples/StyledHeader";
-import {DangerButton, SuccessButton} from "./components/examples/PropsButton";
-import {createGlobalStyle} from "styled-components";
-import AlternativeHeader from "./components/examples/AlternativeHeader";
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    margin: 2rem;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-  }
-`
 
 function App() {
     return (
         <div>
-            <GlobalStyle/>
-            <StyledHeader title="hello from app js"/>
-            <br/>
-            <AlternativeHeader title="hello from alternative header"/>
-            <DangerButton big>danger button</DangerButton>
-            <SuccessButton>success button</SuccessButton>
+            Hello from App
         </div>
     );
 }
