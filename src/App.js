6379d66c4689344f7b21eda814773483b50b1aae
dennis githinji <wakahiad@gmail.com>
2023-03-30 12:26:47 +0300

    media queries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import {Banner, SecondBanner} from "./components/Banners";
 
 const App = () => {
 
     return (
-      <div>
-          hello from app
-      </div>
+        <div>
+            <Banner>
+                <h1>First Banner</h1>
+            </Banner>
+            <SecondBanner>
+                <h1>Second Banner</h1>
+            </SecondBanner>
+        </div>
     )
 };
 
