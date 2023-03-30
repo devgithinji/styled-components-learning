fda1d300548a19033255222b611bc49cd6dacdee
dennis githinji <wakahiad@gmail.com>
2023-03-30 12:19:13 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import './App.css';
-import {ThemeProvider} from "styled-components";
-import {blueTheme, greenTheme, redTheme} from "./components/themes";
-import Navbar from "./components/Navbar";
-import Button from "./components/Button";
-import Content from "./components/Content";
-import {useState} from "react";
 
 const App = () => {
-    const [theme, setTheme] = useState(redTheme)
-
-    const toggleTheme = () => {
-        if (theme.primaryColor === 'red') {
-            setTheme(greenTheme);
-        } else if (theme.primaryColor === 'green') {
-            setTheme(blueTheme);
-        } else {
-            setTheme(redTheme);
-        }
-    }
 
     return (
-        <ThemeProvider theme={theme}>
-            <main>
-                <Navbar>
-                    <Button onClick={toggleTheme}>toggle theme</Button>
-                </Navbar>
-                <Content/>
-            </main>
-        </ThemeProvider>
+      <div>
+          hello from app
+      </div>
     )
 };
 
